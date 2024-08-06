33edb2492221ef690dd86b533dc35cb2f5bc8f91
Adiel Pereira <adielp16@gmail.com>
2024-08-06 13:20:33 -0300

    add tab navigation

--- App.tsx
+++ App.tsx
@@ -2,17 +2,17 @@ import "@/styles/global.css"
 
 import { StatusBar } from "expo-status-bar"
 import { ToastProvider } from "@/components/Toast"
-
-import { Profile } from "@/app/Profile"
-import { HomePage } from "@/components/pages/home/page"
-import { BooksProvider } from "./src/contexts/book.context"
+import { BooksProvider } from "@/contexts/book.context"
+import { TabNavigationOrganism } from "@/components/organisms/tab-navigation.organism"
+import { DarkTheme, NavigationContainer } from "@react-navigation/native"
 
 export default function App() {
   return (
     <ToastProvider position="top">
       <BooksProvider>
-        {/* <Profile /> */}
-        <HomePage/>
+        <NavigationContainer theme={DarkTheme} >
+          <TabNavigationOrganism/>
+        </NavigationContainer>
       </BooksProvider>
       <StatusBar style="light" />
     </ToastProvider>
---
+++ src/components/organisms/tab-navigation.organism.tsx
@@ -0,0 +1,44 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomePage } from '@/components/pages/home/page';
+import { colors } from '@/styles/colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigationOrganism = () => {
+	return (
+    <Tab.Navigator 
+      initialRouteName='Home'
+      screenOptions={{
+        tabBarActiveTintColor: colors.green[400],
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarIconStyle:{
+          width: 32,
+          height: 32,
+        },
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+        }
+      }}
+      >
+      <Tab.Screen name="Home" component={HomePage}  options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="bookshelf" color={color} size={size} />
+        ),
+        }}/>
+      <Tab.Screen name="Scan" component={HomePage}  options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="line-scan" color={color} size={size} />
+        ),
+        }}/>
+      <Tab.Screen name="Wishlist" component={HomePage}  options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="tag-heart" color={color} size={size} />
+        ),
+        }}/>
+    </Tab.Navigator>
+	)
+}
+
+export { TabNavigationOrganism };
